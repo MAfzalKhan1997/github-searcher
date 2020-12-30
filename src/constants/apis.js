44c862f2bad01383bb74e6cf0918b35a33bb3141
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-12-30 20:03:32 +0500

    eslint changes

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'https://api.github.com';
-const USERS_ENDPOINT = '/search/users';
-const REPOSITORIES_ENDPOINT = '/search/repositories';
+const BASE_URL = "https://api.github.com";
+const USERS_ENDPOINT = "/search/users";
+const REPOSITORIES_ENDPOINT = "/search/repositories";
 
 const API = { BASE_URL, USERS_ENDPOINT, REPOSITORIES_ENDPOINT };
 
